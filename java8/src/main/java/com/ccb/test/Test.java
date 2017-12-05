a4912f33f0060d8593229b2678322b93dd79b9cc
hanjiyuan.zh <hanjiyuan.zh@hanjiyuanzh-PC>
2017-12-05 19:33:25 +0800

    modify build path

--- java8/src/main/java/com/ccb/test/Test.java
+++ java8/src/main/java/com/ccb/test/Test.java
@@ -3,7 +3,7 @@ package com.ccb.test;
 public class Test {
 
 	public static void main(String[] args) {
-		MyClassLoader loader3 = new MyClassLoader("D:\\test"); 
+		MyClassLoader loader3 = new MyClassLoader("E:\\test"); 
 		try {
 			Class classN3 = loader3.loadClass("test.N",true);
 			Class.forName("test.N");
