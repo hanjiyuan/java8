c47bb94748eae8dc4d6855156f3c19f49682f6e6
hanjiyuan.zh <hanjiyuan.zh@hanjiyuanzh-PC>
2018-02-28 14:31:47 +0800

    inherit test code.

---
+++ java8/src/main/java/h3w/java6/inherit/Base.java
@@ -0,0 +1,12 @@
+package h3w.java6.inherit;
+
+public class Base {
+	
+	public static String s = "static base";
+	
+	public String m = "member of base";
+	
+	public static void staticTest() {
+		System.out.println("base static: " + s);
+	}
+}
---
+++ java8/src/main/java/h3w/java6/inherit/Child.java
@@ -0,0 +1,12 @@
+package h3w.java6.inherit;
+
+public class Child extends Base {
+
+	public static String s = "static child";
+
+	public String m = "member of child";
+
+	public static void staticTest() {
+		System.out.println("child static: " + s);
+	}
+}
